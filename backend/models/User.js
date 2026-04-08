189f81d2045fcb144a401d41e19fa662f9350807
Ramakrishna046 <163405658+Ramakrishna046@users.noreply.github.com>
2026-04-08 15:45:41 +0530

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  rollNumber: { type: String, unique: true },
-  password: { type: String },
-  section: { type: String, enum: ['IT-1', 'IT-2', 'IT-3'] }
-});
-module.exports = mongoose.model('User', userSchema);
--- backend/utils/rollUtils.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  sectionFromRoll: function (roll) {
-    const num = parseInt(roll.replace(/^0+/, ''));
-    if ((num >= 160123737001 && num <= 160123737070) || (num >= 160123737301 && num <= 160123737307)) return "IT-1";
-    if ((num >= 160123737071 && num <= 160123737137) || (num >= 160123737308 && num <= 160123737314)) return "IT-2";
-    if ((num >= 160123737138 && num <= 160123737220) || (num >= 160123737315 && num <= 160123737321)) return "IT-3";
-    return "";
-  }
-};
